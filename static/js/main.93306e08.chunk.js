34a623566a9d89fc2a97561fd104e158dceb8a12
William Jones <willhomemac@Williams-iMac-2.cust.communityfibre.co.uk>
2024-07-10 18:49:41 +0100

    Updates

---
+++ static/js/main.93306e08.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwhiteboard-app-frontend"]=this["webpackJsonpwhiteboard-app-frontend"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var r=n(1),c=n.n(r),i=n(3),o=n.n(i),d=(n(8),n(0));var a=function(){return Object(d.jsx)("div",{children:Object(d.jsx)("h1",{children:"Whiteboard App"})})};o.a.render(Object(d.jsx)(c.a.StrictMode,{children:Object(d.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.93306e08.chunk.js.map
